4ec64871f00560eb67bdddb3100b895982653b28
Ken Dauer <kennethdauer@gmail.com>
2013-12-03 21:19:33 -0500

    Wow. a lot of things came together really quickly. sketching this out first on paper was a good idea. the board loads, all random colors, then switches to a black board after seven seconds. it then allows the player to change the colors and then tests to see if the changed colors are the same as the originals when the done button is clicked. still needs a lot of cleanup, but most of the functionality is there.

--- js/card-o-matic.js
+++ js/card-o-matic.js
@@ -1,6 +1,4 @@
-$( document ).ready(function() {
-
-  var colors = ["red", "green", "blue"];
+  var colors = ["rgb(255, 0, 0)", "rgb(0, 128, 0)", "rgb(0, 0, 255)"];
   var piece1Color = parseInt(Math.random()*(colors.length));
   var piece2Color = parseInt(Math.random()*(colors.length));
   var piece3Color = parseInt(Math.random()*(colors.length));
@@ -10,6 +8,10 @@ $( document ).ready(function() {
   var piece7Color = parseInt(Math.random()*(colors.length));
   var piece8Color = parseInt(Math.random()*(colors.length));
   var piece9Color = parseInt(Math.random()*(colors.length));
+
+$( document ).ready(function() {
+
+
   	console.log(colors[piece1Color]);
   	console.log(colors[piece2Color]);
   	console.log(colors[piece3Color]);
@@ -28,8 +30,15 @@ $( document ).ready(function() {
   $('#piece7').css('background-color', colors[piece7Color]);
   $('#piece8').css('background-color', colors[piece8Color]);
   $('#piece9').css('background-color', colors[piece9Color]);
+  
+  setTimeout(blackBoard, 7000);
+
 
 });
+
+function blackBoard() {
+	    $('.piece').css('background-color', "black");
+}
 
 $('.piece').click(function() {
 
@@ -41,6 +50,7 @@ $('.piece').click(function() {
 	
 	console.log(currentColor);
 	console.log(red);
+	  	console.log(colors[piece9Color]);
 	
 	if (currentColor == red) {
 		$(this).css('background-color', "green");
@@ -52,4 +62,35 @@ $('.piece').click(function() {
 	$(this).css('background-color', "red");
 	}
 })
+
+$('#done').click(function() {
+
+	var piece1CurrentColor = $('#piece1').css('background-color');
+	var piece2CurrentColor = $('#piece2').css('background-color');
+	var piece3CurrentColor = $('#piece3').css('background-color');
+	var piece4CurrentColor = $('#piece4').css('background-color');
+	var piece5CurrentColor = $('#piece5').css('background-color');
+	var piece6CurrentColor = $('#piece6').css('background-color');
+	var piece7CurrentColor = $('#piece7').css('background-color');
+	var piece8CurrentColor = $('#piece8').css('background-color');
+	var piece9CurrentColor = $('#piece9').css('background-color');
+	
+		console.log(piece1CurrentColor);
+		console.log(colors[piece1Color]);
+
+	if (piece1CurrentColor == colors[piece1Color] && piece2CurrentColor == colors[piece2Color] 
+	&& piece3CurrentColor == colors[piece3Color] && piece4CurrentColor == colors[piece4Color]
+	&& piece5CurrentColor == colors[piece5Color] && piece6CurrentColor == colors[piece6Color]
+	&& piece7CurrentColor == colors[piece7Color] && piece8CurrentColor == colors[piece8Color]
+	&& piece9CurrentColor == colors[piece9Color]
+	
+	) {
+		$('#checker').html("You win!");
+	}
+	else {
+		$('#checker').html("You lose!");
+	}
+})
+
+
 
